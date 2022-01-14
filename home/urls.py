73f670b4778dc4593825df1cf7c8a2fcb0da29be
pratigya paudel <pratigyapaudel0@gmail.com>
2022-01-14 19:31:24 +0545

    new code

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+urlpatterns = [
+path("",views.index,name='home'),
+path("market",views.market,name='market'),
+path("news",views.news,name='News')
+]
