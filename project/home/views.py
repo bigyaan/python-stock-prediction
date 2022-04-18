041376d008a65029c2af8c5add9c0caa4fa3b5b1
prash00b <prashamsa068@gmail.com>
2022-04-18 10:34:03 +0545

    prashamsa changes

--- project/home/views.py
+++ project/home/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render,HttpResponse
-from pyint import spiketime
-from pyint.config import config_spiketime
+import json
 
 # Create your views here.
 def index(request):
-    data = spiketime.TimeSlot()
-    print(data)
-    return render (request,'index.html',{'data':data})
+    # data = spiketime.TimeSlot()
+   
+    return render (request,'index.html')
    #return HttpResponse("This is home page")
 def market(request):
-    return render (request,'market.html')
+    f = open('pyint//stock_pre_output.json')
+    data = json.load(f)
+    # Iterating through the json
+    # # list
+    # for i in data['A']:
+    #     print(i)
+    # print(data)
+    # print (data['A']['spike_time'])
+    return render (request,'market.html',{'market':data})
 def news(request):
     return render (request,'news.html')
